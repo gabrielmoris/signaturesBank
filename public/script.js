73efa8d8e02541306d2eb6b8e7825062b6bd6315
Gabriel C Moris <gabrieltrompeta@gmail.com>
2021-12-17 12:49:42 +0100

    improvements on touchscreen

--- public/script.js
+++ public/script.js
@@ -4,8 +4,8 @@
     let context = canv.getContext("2d");
     let dataURL;
     let isSigning = false;
-    const canvPosX = canv.offsetLeft + canv.offsetWidth / 2;
-    const canvPosY = canv.offsetTop + canv.offsetHeight * 2;
+    const canvPosX = canv.offsetLeft + canv.offsetWidth * 2;
+    const canvPosY = canv.offsetTop + canv.offsetHeight / 2;
     let xpos = 0;
     let ypos = 0;
 
